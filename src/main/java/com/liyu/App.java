0ae5b773a814b9df3139be2967f2a20945c6e2f6
cafebabe <18508227568@163.com>
2016-07-30 10:09:36 +0800

    hello world

---
+++ src/main/java/com/liyu/App.java
@@ -0,0 +1,13 @@
+package com.liyu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
